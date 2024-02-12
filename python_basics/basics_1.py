8e71073d0bb5b7357313f25db277dfe74b18520f
akashmishra0703 <159708071+akashmishra0703@users.noreply.github.com>
2024-02-12 16:10:00 +0000

    day_1 python basics

---
+++ python_basics/basics_1.py
@@ -0,0 +1,16 @@
+import os
+# print(os.getcwd())
+print("Hello Everyone")
+
+def to_connect(n):
+    print(n)
+
+to_connect(6)
+
+
+# python - Byte_code ==== python vm
+# python vertual machine is a software runnong a loop for run the code
+
+
+# mutable and immutable
+# referecnce will not change 
---
+++ python_basics/basics_2.py
@@ -0,0 +1,17 @@
+from basics_1 import to_connect
+import os
+import sys
+os.getcwd()
+
+sys.platform
+to_connect(5)
+
+
+
+string = "India"
+for i in string:
+    print(i)
+
+
+
+print(sys.platform)
